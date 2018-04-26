dda6ba51b07adb49feef359cc5051416189756be
Caren Keyes <cejkeyes@gmail.com>
2018-04-26 15:54:26 -0700

    add put endpoint

--- server.js
+++ server.js
@@ -63,6 +63,26 @@ app.post('/blogpost', (req,res) =>{
     
 });
 
+app.put('/blogpost/:id', (req,res) =>{
+    if(!(req.body.id && req.params.id === req.body.id)){
+        const message = `Request path id ${req.params.id} and request body id ${req.body.id} must match`;
+        console.error(message);
+        return res.status(400).json({message: message});
+
+    }
+    const toUpdate = {};
+    const updateableFields = ['title', 'author', 'content']
+    updateableFields.forEach(field => {
+        if (field in req.body){
+            toUpdate[field] = req.body[field];
+        }
+    });
+    blogPost
+        .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+        .then(restaurant => res.status(204).end())
+        .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
 function runServer(databaseUrl, port=PORT){
     return new Promise((resolve, reject) =>{
         mongoose.connect(databaseUrl, err =>{
